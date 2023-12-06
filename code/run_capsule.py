585498a8249f0268138f6b64cfbb9fc61b4402ce
Arielle Leon <ariellel@alleninstitute.org>
2023-12-06 20:45:09 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -142,7 +142,6 @@ def check_non_rigid_registration(input_dir, oeid):
     processing_json = next(input_dir.glob("processing.json"))
     with open(processing_json, "r") as f:
         pj = json.load(f)
-    import pdb;pdb.set_trace()
     if pj['processing_pipeline']["data_processes"][0]["parameters"].get("nonrigid", False):
         return True
     else:
