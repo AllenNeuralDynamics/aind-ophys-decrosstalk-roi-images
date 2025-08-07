5c21d1fbb6cf6f2058d7db47a7ebd6106edf6704
arielleleon <54869022+arielleleon@users.noreply.github.com>
2025-08-07 13:13:52 -0700

    fix: debug default to False (#35)

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -442,7 +442,7 @@ def get_frame_rate(session_fp: Path) -> float:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--debug", action="store_true", default=False, help="Enable debug mode")
     args = parser.parse_args()
     input_dir = Path("../data/").resolve()
     output_dir = Path("../results/").resolve()
